ddad77e609db2cea98ab1de94e7ab57ba3163133
rjspyk5 <rjspyk5@gmail.com>
2024-05-19 02:21:03 +0600

    add simple paralox

--- src/Pages/Home/Featured.jsx
+++ src/Pages/Home/Featured.jsx
@@ -5,7 +5,7 @@ export const Featured = () => {
   return (
     <div
       style={{ backgroundImage: `url(${im})` }}
-      className="my-14 bg-cover  relative"
+      className="my-14 bg-cover bg-fixed relative"
     >
       <div className="my-10">
         <HeadingSection
@@ -13,7 +13,19 @@ export const Featured = () => {
           subheading="---Check it out---"
         />
       </div>
-      <img className="w-52" src={im} alt="" />
+      <div className="md:flex justify-center items-center py-8 px-16">
+        <img className="w-52" src={im} alt="" />
+        <div className="md:ml-10">
+          <p>Aug,2024</p>
+          <p className="uppercase">Where can i get some?</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+            aperiam quod expedita eveniet similique eius temporibus reiciendis
+            nemo modi! Ipsam.
+          </p>
+          <button className="btn btn-outline">Read More</button>
+        </div>
+      </div>
     </div>
   );
 };
