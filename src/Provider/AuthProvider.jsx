13f37c156cde80fd0bfd034faea3baab49554bbc
rjspyk5 <rjspyk5@gmail.com>
2024-05-19 19:03:09 +0600

    add onauthstatechaged in authprovider

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,31 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase.config";
 
 export const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
+  const [user, setuser] = useState(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      currentUser && setuser(currentUser);
+    });
+
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
   const createUser = (email, pass) => {
     return createUserWithEmailAndPassword(auth, email, pass);
   };
-  const contextValues = { createUser };
+  const logIn = (email, pass) => {
+    return signInWithEmailAndPassword(auth, email, pass);
+  };
+  const contextValues = { createUser, logIn, user };
   return (
     <AuthContext.Provider value={contextValues}>
       {children}
