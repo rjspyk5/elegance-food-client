88c0079931a7aebcfe2b3c6c23edd003afba36fa
rjspyk5 <rjspyk5@gmail.com>
2024-05-20 17:22:48 +0600

    paralox blur effect power change

--- src/Pages/OurMenu/MenuCategory.jsx
+++ src/Pages/OurMenu/MenuCategory.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { MenuIteam } from "../Home/Shared/MenuIteam";
 import { Cover } from "../Home/Shared/Cover";
 import { Link } from "react-router-dom";
-import { Parallax, Background } from "react-parallax";
+import { Parallax } from "react-parallax";
 
 export const MenuCategory = ({ item, im, title }) => {
   return (
     <div>
       {title && (
-        <Parallax blur={{ min: -15, max: 50 }} bgImage={im} strength={200}>
-          <Cover im={im} title={title} />
+        <Parallax blur={{ min: -15, max: 20 }} bgImage={im} strength={10}>
+          <Cover title={title} />
         </Parallax>
       )}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
