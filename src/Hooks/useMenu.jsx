95948dadd5d180fc1a4e0f6478e29f47771290f4
rjspyk5 <rjspyk5@gmail.com>
2024-05-20 23:41:50 +0600

    load catagorywise food in ourshop page

--- src/Hooks/useMenu.jsx
+++ src/Hooks/useMenu.jsx
@@ -4,7 +4,7 @@ export const useMenu = () => {
   const [menu, setmenu] = useState([]);
   const [loading, setloading] = useState(true);
   useEffect(() => {
-    fetch(`menu.json`)
+    fetch(`/menu.json`)
       .then((res) => res.json())
       .then((res) => {
         setmenu(res);
--- src/Pages/OurShop/OurShop.jsx
+++ src/Pages/OurShop/OurShop.jsx
@@ -3,10 +3,20 @@ import "react-tabs/style/react-tabs.css";
 import { Cover } from "../Home/Shared/Cover";
 import bg from "../../assets/shop/banner2.jpg";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMenu } from "./../../Hooks/useMenu";
+import { FoodCard } from "../../Component/FoodCard";
 export const OurShop = () => {
   const { category } = useParams();
   const [tabindex, settabindex] = useState(1);
+
+  const [menu] = useMenu();
+
+  const drinks = menu.filter((e) => e.category === "drinks");
+  const soup = menu.filter((e) => e.category === "soup");
+  const salad = menu.filter((e) => e.category === "salad");
+  const pizza = menu.filter((e) => e.category === "pizza");
+
   return (
     <div>
       <Cover title={`Order Food`} im={bg} />
@@ -18,10 +28,26 @@ export const OurShop = () => {
           <Tab>Drink</Tab>
         </TabList>
         <TabPanel>
-          <h2>Any content 1</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+            {salad.map((el) => (
+              <FoodCard key={el._id} item={el} />
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          {pizza.map((el) => (
+            <FoodCard key={el._id} item={el} />
+          ))}
+        </TabPanel>
+        <TabPanel>
+          {soup.map((el) => (
+            <FoodCard key={el._id} item={el} />
+          ))}
+        </TabPanel>
+        <TabPanel>
+          {drinks.map((el) => (
+            <FoodCard key={el._id} item={el} />
+          ))}
         </TabPanel>
       </Tabs>
     </div>
