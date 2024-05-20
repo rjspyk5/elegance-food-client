ed9e83cca53608233125de7406a5a64d1f5828e5
rjspyk5 <rjspyk5@gmail.com>
2024-05-20 17:16:06 +0600

    add simple react parallaox

--- src/Pages/OurMenu/MenuCategory.jsx
+++ src/Pages/OurMenu/MenuCategory.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { MenuIteam } from "../Home/Shared/MenuIteam";
 import { Cover } from "../Home/Shared/Cover";
 import { Link } from "react-router-dom";
+import { Parallax, Background } from "react-parallax";
 
 export const MenuCategory = ({ item, im, title }) => {
   return (
     <div>
-      {title && <Cover im={im} title={title} />}
+      {title && (
+        <Parallax blur={{ min: -15, max: 50 }} bgImage={im} strength={200}>
+          <Cover im={im} title={title} />
+        </Parallax>
+      )}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {item.map((el) => (
           <MenuIteam key={el._id} menu={el} />
