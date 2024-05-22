421fd3fc38b0050b0f069c4297c4e0b6cad10e55
rjspyk5 <rjspyk5@gmail.com>
2024-05-22 23:35:38 +0600

    make simple add cart function

--- src/Component/FoodCard.jsx
+++ src/Component/FoodCard.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useAuth } from "../Hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
-export const FoodCard = ({ item: { name, price, image, recipe } }) => {
+export const FoodCard = ({ item }) => {
+  const { name, price, image, recipe } = item;
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const handleAddCart = () => {
+    if (user && user.email) {
+      console.log(item);
+    } else {
+      navigate("/login");
+    }
+  };
   return (
     <div className="relative">
       <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -11,7 +23,9 @@ export const FoodCard = ({ item: { name, price, image, recipe } }) => {
           <h2 className="text-2xl font-bold text-center">{name}</h2>
           <p>{recipe}</p>
           <div className="flex justify-center">
-            <button className="btn btn-primary">Add to Cart</button>
+            <button onClick={handleAddCart} className="btn btn-primary">
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
