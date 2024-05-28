6eeb73a0b74484b94f3be862fe4dd616081eaa62
rjspyk5 <rjspyk5@gmail.com>
2024-05-29 01:49:37 +0600

    setup axios sequre

--- src/Hooks/useAxiosSequre.jsx
+++ src/Hooks/useAxiosSequre.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useAuth } from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSequre = axios.create({
   baseURL: "http://localhost:5000",
@@ -6,5 +8,18 @@ const axiosSequre = axios.create({
 });
 
 export const useAxiosSequre = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      if (error.response.status === 401 || error.response.status === 403) {
+        logOut().then(() => navigate("/"));
+      }
+      return Promise.reject(error);
+    }
+  );
   return axiosSequre;
 };
